84215d18544e132eafee6c6a13a441311ba047e6
invtfl-yuvraj <yuvrajsingh3715@gmail.com>
2025-12-07 07:43:09 +0530

    Basic TS and express setup done

---
+++ src/config/server.config.ts
@@ -0,0 +1,6 @@
+import dotenv from "dotenv";
+dotenv.config({quiet: true});
+
+export default {
+  PORT: process.env.PORT || 3000
+};
---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express from "express"
+import serverConfig from "./config/server.config.js";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+app.listen(serverConfig.PORT, () => {
+  console.log(`Server is running on port ${serverConfig.PORT}`);
+});
+
+
